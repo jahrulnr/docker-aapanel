6f677c705caf884611cbd7ca67dacd8da6f5db68
Jahrulnr <jahrulnr@gmail.com>
2023-11-24 14:42:54 +0700

    fix some issue

--- vm/start.sh
+++ vm/start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Move instalation to volume
+if [ -d "/www/server-base" ]; then
+    rm -r /www/server/*
+    yes | cp -rf /www/server-base/* /www/server/
+    rm -r /www/server-base
+fi
+
 # Start supervisord and services
 /www/server/panel/pyenv/bin/supervisord -n -c /etc/supervisord.conf
