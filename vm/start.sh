0b6e12faeb1159b3d8d32ab3c09eb3abc1fe6d81
Jahrulnr <jahrulnr@gmail.com>
2023-11-27 03:38:08 +0700

    Update some command

--- vm/start.sh
+++ vm/start.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# Move init.d from docker to volume
+if [ ! -f /etc/init.d/bt ]; then
+   yes | cp -rf /tmp/init.d/* /etc/init.d/ 
+fi
+
 # Move instalation to volume
 if [ -d "/www/server-base" ] && [ ! -d /www/server/panel ]; then
     rm -rf /www/server/*
     yes | cp -rf /www/server-base/* /www/server/
-    rm -rf /www/server-base
 
-    chown -R www:www /www
+    chown -R root:root /www/server
+    chown -R www:www /www/wwwroot
+fi
+
+if [ -d "/www/server-base" ]; then
+    rm -rf /www/server-base
 fi
 
 # Start supervisord and services
